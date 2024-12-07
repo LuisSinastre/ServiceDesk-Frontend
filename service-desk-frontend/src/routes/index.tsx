dbe159b7981f2fc5c175dda8621824471a97bd14
LuisSinastre <luisgustavosinastre13@gmail.com>
2024-12-07 16:05:29 -0300

    feat: Correction and addition of page navigation contexts

--- service-desk-frontend/src/routes/index.tsx
+++ service-desk-frontend/src/routes/index.tsx
@@ -1,6 +1,6 @@
 // src/routes/index.tsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "../pages/login";
 import Home from "../pages/home/home"; // Corrigindo para importar como default
 import { AuthProvider } from "../contexts/AuthContext";
@@ -10,8 +10,11 @@ const App = () => {
     <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
+
+          {/* Rota para links inválidos */}
+          <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
       </AuthProvider>
     </Router>
