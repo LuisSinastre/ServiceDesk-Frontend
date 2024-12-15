70359f5e9e57626962a8f913a3f78062af743451
LuisSinastre <luisgustavosinastre13@gmail.com>
2024-12-15 02:31:13 -0300

    feat: finishing approval page and roles

---
+++ service-desk-frontend/src/pages/approval/FormField.tsx
@@ -0,0 +1,17 @@
+// src/pages/approval/FormField.tsx
+import React from 'react';
+
+interface FormFieldProps {
+  label: string;
+  value: any;
+}
+
+const FormField: React.FC<FormFieldProps> = ({ label, value }) => {
+  return (
+    <p>
+      <strong>{label}:</strong> {value || "N/A"}
+    </p>
+  );
+};
+
+export default FormField;
